117c7627703e1e24c1efe6dabd5caa46518bf622
Simone Raffoni <SIMONE.RAFFONI@local.ittcesena.it>
2026-03-06 09:57:35 +0100

    Inizio del progetto con funzione async, realizzato il bottone estrazione e realzizata una label dove appaiono le lettere in modo randomico. Per la prossima volta nel progetto si dive fare in modo che le lettere generate siano una dopo l'altra e anche che l'utente possa scegliere quante lettere avere. Infine migliorare la grafica

---
+++ RandomNumber/RandomNumber/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomNumber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        private string[] Alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+        string parola;
+        private async void btnEstrazione_Click(object sender, RoutedEventArgs e)
+        {
+        while (true)
+            {
+                string estrazioneCasuale = Alfabeto[rnd.Next(0, 25)];
+                parola += estrazioneCasuale;
+                lblLettera.Content = estrazioneCasuale;
+
+                string label = estrazioneCasuale;
+                await Task.Delay(400);
+            }   
+        }
+    }
+}
